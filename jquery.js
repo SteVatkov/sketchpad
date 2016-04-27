efbced3723bba1ef13df9ba850c311985822388e
Steve <stevatkov@gmail.com>
2016-04-27 17:17:45 -0400

    Added grid of boxes, prompt input, and change colour on hover and click

--- jquery.js
+++ jquery.js
@@ -1,4 +1,41 @@
+var count = 0;
+var size = 32;
+
+function createBox() {
+	size = prompt('Enter a size');
+
+	var boxSize = $('.wrapper').width()/size;
+
+	for(var i=0; i < size * size; i++){
+		$('.wrapper').append('<div class="box"></div>');
+	}
+
+	$('.box').css('width', boxSize);
+	$('.box').css('height', boxSize);
+
+}
+
+function hoverColor(){
+
+	$('.box').on("mouseenter", function(){
+		
+		
+		$(this).fadeTo(100,0);
+		$(this).mouseleave(function(){
+			$(this).fadeTo(400,1);
+		});
+
+		$(this).on("click",function(){
+			$(this).toggleClass("boxSelect");
+
+		});
+	});
+
+}
+
+
 $(document).ready(function(){
 
-
+	createBox();
+	hoverColor();
 });
